7c5047a8e9c5fd57f9e3937b1cd8437652b020d6
Byung Hyun Choi <choi8608@gmail.com>
2020-04-08 16:52:33 +0900

    [BE] Feature: login Test 를 위한 Swagger 구성
    - Swagger 에 Authorization 접근 추가

--- src/main/java/com/codesquad/todo/config/SwaggerConfig.java
+++ src/main/java/com/codesquad/todo/config/SwaggerConfig.java
@@ -1,14 +1,20 @@
 package com.codesquad.todo.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -30,6 +36,26 @@ public class SwaggerConfig {
         .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.ant("/api/**"))
         .build()
-        .apiInfo(metadata());
+        .apiInfo(metadata())
+        .securityContexts(Lists.newArrayList(securityContext()))
+        .securitySchemes(Lists.newArrayList(apiKey()));
+  }
+
+  private springfox.documentation.spi.service.contexts.SecurityContext securityContext() {
+    return springfox.documentation.spi.service.contexts.SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .forPaths(PathSelectors.any())
+        .build();
+  }
+
+  private ApiKey apiKey() {
+    return new ApiKey("JWT", "Authorization", "header");
+  }
+
+  List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
   }
 }
---
+++ src/main/java/com/codesquad/todo/message/ErrorMessages.java
@@ -0,0 +1,5 @@
+package com.codesquad.todo.message;
+
+public class ErrorMessages {
+  public static final String ERROR = "ERROR";
+}
