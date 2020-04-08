0f6d6854f698c6aaff7fea449c927af3a2c5ed4e
Byung Hyun Choi <choi8608@gmail.com>
2020-04-08 16:45:00 +0900

    [BE] Feature: login 을 위한 JWT 구현
    - JWT generate
      - setExpiration : 10 minute
    - JWT verify
    
    #35

--- src/main/java/com/codesquad/todo/service/AuthService.java
+++ src/main/java/com/codesquad/todo/service/AuthService.java
@@ -1,17 +1,65 @@
 package com.codesquad.todo.service;
 
+import com.codesquad.todo.message.AuthMessages;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 public class AuthService {
 
-  public Map<String, Object> login() {
+  private final long MINUTE_10 = (1000L * 60L * 10L);
+
+  public Map<String, Object> login(String userId) {
     Map<String, Object> result = new HashMap<>();
-    result.put("contents", "login sucess");
+    result.put("loginToken", create(userId));
 
     return result;
+  }
+
+  public boolean verifyToken(String token) {
+    try {
+      Jws<Claims> claims = Jwts.parser()
+          .setSigningKey(this.generateKey())
+          .parseClaimsJws(token);
+
+      return true;
+    } catch (Exception e) {
+      log.error("Making JWT Key Error ::: {}", e.getMessage());
+
+      return false;
+    }
+  }
+
+  private String create(String value) {
+
+    String jwt = Jwts.builder()
+        .setHeaderParam("typ", "JWT")
+        .setIssuer("Dan")
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + MINUTE_10))
+        .setSubject("loginToken")
+        .claim("userId", value)
+        .signWith(SignatureAlgorithm.HS256, this.generateKey())
+        .compact();
+    return jwt;
+  }
+
+  private byte[] generateKey() {
+    try {
+      return AuthMessages.SALT.getBytes("UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 }
