ef642c209ee16d9c401da9a2461389de5ee8f1fa
Lee SeEun <sally4405@naver.com>
2020-04-09 14:15:43 +0900

    [FE] Setup: 개발환경 구성
    - webpack 설정
    - util 파일, css reset 파일 추가
    
    #5

---
+++ FE/src/util/dom.js
@@ -0,0 +1,27 @@
+export function getElement(target) {
+  return document.querySelector(target);
+}
+
+export function getElements(target) {
+  return document.querySelectorAll(target);
+}
+
+export function addClass(target, className) {
+  return target.classList.add(className);
+}
+
+export function removeClass(target, className) {
+  return target.classList.remove(className);
+}
+
+export function hasClass(target, className) {
+  return target.classList.contains(className);
+}
+
+export function show(target) {
+  target.style.display = 'block';
+}
+
+export function hide(target) {
+  target.style.display = 'none';
+}
