18f554e874e0f5a3e7ee266b48da15d9638badd8
Byung Hyun Choi <choi8608@gmail.com>
2020-04-09 15:47:48 +0900

    [BE] Refactor: note 로직 개선
    - Validation 적용
    - @ResponseStatus 사용을 통한 Controller 간소화
    
    #32

--- src/main/java/com/codesquad/todo/controller/NoteController.java
+++ src/main/java/com/codesquad/todo/controller/NoteController.java
@@ -8,9 +8,10 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
@@ -22,28 +23,30 @@ public class NoteController {
   NoteService noteService;
 
   @ApiOperation(value = "", notes = "Get all notes", tags = "Mockup")
+  @ResponseStatus(HttpStatus.OK)
   @GetMapping
-  public ResponseEntity<ApiResponse> getAllNotes() {
-    return new ResponseEntity<>(new ApiResponse(SuccessMessages.SUCCESS, noteService.getAllNotes()), HttpStatus.OK);
+  public ApiResponse getAllNotes() {
+    return new ApiResponse(SuccessMessages.SUCCESS, noteService.getAllNotes());
   }
 
   @ApiOperation(value = "", notes = "Create note", tags = "Dev")
+  @ResponseStatus(HttpStatus.OK)
   @PostMapping
-  public ResponseEntity<ApiResponse> createNote(@RequestBody Note note) {
-    return new ResponseEntity<>(new ApiResponse(SuccessMessages.SUCCESS, noteService.createNote(note)), HttpStatus.OK);
+  public ApiResponse createNote(@Valid @RequestBody Note note) {
+    return new ApiResponse(SuccessMessages.SUCCESS, noteService.createNote(note));
   }
 
   @ApiOperation(value = "", notes = "Soft delete note", tags = "Dev")
+  @ResponseStatus(HttpStatus.OK)
   @DeleteMapping
-  public ResponseEntity<ApiResponse> deleteNote(@RequestParam Long id) {
-    return new ResponseEntity<>
-        (new ApiResponse(SuccessMessages.SUCCESS, noteService.deleteNote(id)), HttpStatus.OK);
+  public ApiResponse deleteNote(@RequestParam Long id) {
+    return new ApiResponse(SuccessMessages.SUCCESS, noteService.deleteNote(id));
   }
 
   @ApiOperation(value = "", notes = "Get all notes about specific column", tags = "Dev")
+  @ResponseStatus(HttpStatus.OK)
   @GetMapping("/column")
-  public ResponseEntity<ApiResponse> getSpecificColumnNotes(@RequestParam String columnName) {
-    return new ResponseEntity<>
-        (new ApiResponse(SuccessMessages.SUCCESS, noteService.getSpecificColumnNotes(columnName)), HttpStatus.OK);
+  public ApiResponse getSpecificColumnNotes(@RequestParam String columnName) {
+    return new ApiResponse(SuccessMessages.SUCCESS, noteService.getSpecificColumnNotes(columnName));
   }
 }
