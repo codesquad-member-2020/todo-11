5c70ff903c63313d0be979b7c5a042c82ac03477
Lee SeEun <sally4405@naver.com>
2020-04-09 14:50:44 +0900

    [FE] Feature: HTML/CSS
    - column, note 구조를 작성
    - column의 + 버튼 클릭시 add note 기능 온오프 되도록 구현
    
    # 6

--- FE/src/main.js
+++ FE/src/main.js
@@ -1,15 +1,17 @@
 import { css } from '../css/style.css';
+import { getElement, show, hide } from './util/dom.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const result = document.querySelector('.result');
-    const plusButton = document.querySelector('.plus-button');
-    const minusButton = document.querySelector('.minus-button');
-    
-    plusButton.addEventListener('click', () => {
-        result.innerHTML++;
-    });
-
-    minusButton.addEventListener('click', () => {
-        result.innerHTML--;
-    });
+  getElement('.column-menu_addBtn').addEventListener('click', () => {
+    const addNote = getElement('.add-note');
+    const addNoteDisplay = addNote.style.display;
+    switch (addNoteDisplay) {
+      case 'block': hide(addNote);
+        break;
+      case "": show(addNote);
+        break;
+      case 'none': show(addNote);
+        break;
+    }
+  })
 });
