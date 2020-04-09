4ac051c5be2e727ab02d9063ace3bba8a3e86e35
Byung Hyun Choi <choi8608@gmail.com>
2020-04-09 16:58:30 +0900

    [BE] Feature: 예외 처리
    - ErrorResponse.java
      - 에러 처리 공통 Bean
    - 예외 처리 추가
      - HTTP_MESSAGE_NOT_READABLE_EXCEPTION
      - METHOD_ARGUMENT_NOT_VALID_EXCEPTION
    
    #38

--- src/main/java/com/codesquad/todo/message/ErrorMessages.java
+++ src/main/java/com/codesquad/todo/message/ErrorMessages.java
@@ -3,5 +3,7 @@ package com.codesquad.todo.message;
 import org.springframework.http.HttpStatus;
 
 public class ErrorMessages {
-  public static final String INTERNAL_SERVER_ERROR = HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase();
+  public static final String EXCEPTION = HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase();
+  public static final String HTTP_MESSAGE_NOT_READABLE_EXCEPTION = "요청 값을 이해할 수 없습니다";
+  public static final String METHOD_ARGUMENT_NOT_VALID_EXCEPTION = "요청 값이 적절하지 않습니다";
 }
