34c73ed313cac95af1e163d74ad7be8c35f414f1
Hyune-c <55722186+Hyune-c@users.noreply.github.com>
2020-04-10 18:27:25 +0900

    Revert "Revert "[Be] CORS 보완 & 리팩토링""

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE note
     column_name VARCHAR(100)  NOT NULL,
     content     VARCHAR(1000) NOT NULL,
     created_at  TIMESTAMP     NOT NULL DEFAULT current_timestamp,
-    updated_at  TIMESTAMP     NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
+    updated_at  TIMESTAMP     NOT NULL DEFAULT current_timestamp,
     user_id     VARCHAR(32)   NOT NULL,
     deleted     BOOLEAN       NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
