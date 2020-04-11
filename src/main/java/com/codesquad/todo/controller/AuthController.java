770af1082d61953a57aaa1e9438a35eb0dfcb3a3
Byung Hyun Choi <choi8608@gmail.com>
2020-04-12 02:05:07 +0900

    [BE] Refactor: login url 개선
    - login url 개선
    
    #35

--- src/main/java/com/codesquad/todo/controller/AuthController.java
+++ src/main/java/com/codesquad/todo/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.codesquad.todo.controller;
 import com.codesquad.todo.bean.ApiResponse;
 import com.codesquad.todo.message.SuccessMessages;
 import com.codesquad.todo.service.AuthService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,8 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @RestController
 @Validated
-@RequestMapping("/api/login")
+@Api(tags = "Dev")
+@RequestMapping("/dev-api/login")
 public class AuthController {
 
   private static final String HEADER_AUTH = "Authorization";
@@ -27,14 +29,14 @@ public class AuthController {
   @Autowired
   AuthService authService;
 
-  @ApiOperation(value = "", notes = "Get JWT token", tags = "Mockup")
+  @ApiOperation(value = "", notes = "Get JWT token")
   @GetMapping
   public ResponseEntity<ApiResponse> login(@RequestParam String userId) {
     return new ResponseEntity<>(new ApiResponse(SuccessMessages.SUCCESS, authService.login(userId)), HttpStatus.OK);
   }
 
-  @ApiOperation(value = "", notes = "Verify JWT token", tags = "Mockup")
-  @GetMapping("verify")
+  @ApiOperation(value = "", notes = "Verify JWT token")
+  @GetMapping("/verify")
   public ResponseEntity<ApiResponse> verifyToken(HttpServletRequest request) {
     if (authService.verifyToken(request.getHeader(HEADER_AUTH))) {
       return new ResponseEntity<>(new ApiResponse(SuccessMessages.SUCCESS, null), HttpStatus.OK);
