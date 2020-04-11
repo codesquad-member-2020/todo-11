313b2c9eae2aa9326b52951ce0abd13e94c0cbea
Byung Hyun Choi <choi8608@gmail.com>
2020-04-12 00:46:24 +0900

    [BE] Feature: Dev 추가
    - Test 를 위한 Controller 추가
    
    #56

---
+++ src/main/java/com/codesquad/todo/dev/DevController.java
@@ -0,0 +1,32 @@
+package com.codesquad.todo.dev;
+
+import com.codesquad.todo.bean.ApiResponse;
+import com.codesquad.todo.message.SuccessMessages;
+import com.codesquad.todo.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Validated
+@Api(tags = "Dev")
+@RequestMapping("/dev-api")
+public class DevController {
+
+  @Autowired
+  CategoryService categoryService;
+
+  @ApiOperation(value = "", notes = "Get all category")
+  @GetMapping("/category")
+  public ResponseEntity<ApiResponse> getAllCategory() {
+    return new ResponseEntity<>(new ApiResponse(SuccessMessages.SUCCESS, categoryService.getAll()), HttpStatus.OK);
+  }
+}
