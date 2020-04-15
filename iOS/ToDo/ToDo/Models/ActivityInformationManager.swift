f2ba6ecec80a6fed906cb545ee9fa7d1ac519919
idevjinie <idevjinie@gmail.com>
2020-04-15 15:43:28 +0900

    [iOS] Feat: ActivityInformationManager 클래스 생성

---
+++ iOS/ToDo/ToDo/Models/ActivityInformationManager.swift
@@ -0,0 +1,30 @@
+//
+//  ActivityInformationManager.swift
+//  ToDo
+//
+//  Created by jinie on 2020/04/15.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import Foundation
+
+class ActivityInformationManager {
+    
+    var activities: [Activity]?
+    
+    func request(_ completion: @escaping () -> ()) {
+        guard let url = URL(string: "http://15.165.223.140:8080/api/history") else { return }
+        let request = URLRequest(url: url)
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+            let decoder = JSONDecoder()
+            guard let responseData = try? decoder.decode(ActivityInformation.self, from: data) else { return }
+            self.activities = responseData.contents.activities
+            print(self.activities)
+            completion()
+        }
+        dataTask.resume()
+    }
+    
+}
